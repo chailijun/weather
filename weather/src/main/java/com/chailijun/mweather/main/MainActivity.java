bcc1dbb1a617b17b9f92687477a36bec058a770f
chailijun <1499505466@qq.com>
2017-03-02 21:02:29 +0800

    修改分享

--- weather/src/main/java/com/chailijun/mweather/main/MainActivity.java
+++ weather/src/main/java/com/chailijun/mweather/main/MainActivity.java
@@ -350,41 +350,14 @@ public class MainActivity extends BaseActivity {
             if (platform.name().equals("WEIXIN_FAVORITE")) {
                 Toast.makeText(mActivity.get(), platform + "收藏成功啦", Toast.LENGTH_SHORT).show();
             } else {
-                if (platform != SHARE_MEDIA.MORE && platform != SHARE_MEDIA.SMS
-                        && platform != SHARE_MEDIA.EMAIL
-                        && platform != SHARE_MEDIA.FLICKR
-                        && platform != SHARE_MEDIA.FOURSQUARE
-                        && platform != SHARE_MEDIA.TUMBLR
-                        && platform != SHARE_MEDIA.POCKET
-                        && platform != SHARE_MEDIA.PINTEREST
-
-                        && platform != SHARE_MEDIA.INSTAGRAM
-                        && platform != SHARE_MEDIA.GOOGLEPLUS
-                        && platform != SHARE_MEDIA.YNOTE
-                        && platform != SHARE_MEDIA.EVERNOTE) {
-                    Toast.makeText(mActivity.get(), platform + "分享成功啦", Toast.LENGTH_SHORT).show();
-                }
+                Toast.makeText(mActivity.get(), platform + "分享成功啦", Toast.LENGTH_SHORT).show();
             }
         }
 
         @Override
         public void onError(SHARE_MEDIA platform, Throwable t) {
-            if (platform != SHARE_MEDIA.MORE && platform != SHARE_MEDIA.SMS
-                    && platform != SHARE_MEDIA.EMAIL
-                    && platform != SHARE_MEDIA.FLICKR
-                    && platform != SHARE_MEDIA.FOURSQUARE
-                    && platform != SHARE_MEDIA.TUMBLR
-                    && platform != SHARE_MEDIA.POCKET
-                    && platform != SHARE_MEDIA.PINTEREST
-
-                    && platform != SHARE_MEDIA.INSTAGRAM
-                    && platform != SHARE_MEDIA.GOOGLEPLUS
-                    && platform != SHARE_MEDIA.YNOTE
-                    && platform != SHARE_MEDIA.EVERNOTE) {
-                Toast.makeText(mActivity.get(), platform + "分享失败啦", Toast.LENGTH_SHORT).show();
-                if (t != null) {
-                    Logger.d("throw", "throw:" + t.getMessage());
-                }
+            if (t != null) {
+                Toast.makeText(mActivity.get(), platform + "分享失败啦:"+t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         }
 
